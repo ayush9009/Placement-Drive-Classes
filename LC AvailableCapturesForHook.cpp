799b41a44ed984ea4398c1ad1066004cd641c37a
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-02-20 20:03:03 +0530

    Create LC AvailableCapturesForHook.cpp

---
+++ LC AvailableCapturesForHook.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int numRookCaptures(vector<vector<char>>& board) {
+        int x,y;
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='R'){
+                    x=i;
+                    y=j;
+                }
+            }
+        }
+        int cnt=0;
+        int j=y,i=x;
+        while(j>=0 && board[i][j]!='B'){
+             if(board[i][j]=='p'){
+                cnt++;
+                break;
+            }
+            j--;
+        }
+        i=x,j=y;
+        while(i>=0 && board[i][j]!='B'){
+            if(board[i][j]=='p'){
+                cnt++;
+                break;
+            }
+            i--;
+        }
+       i=x,j=y;
+       while(i<n && board[i][j]!='B'){
+            if(board[i][j]=='p'){
+                cnt++;
+                break;
+            }
+            i++;
+       }
+        j=y,i=x;
+        while(j<m && board[i][j]!='B'){
+            if(board[i][j]=='p'){
+                cnt++;
+                break;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
