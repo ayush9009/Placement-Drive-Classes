ea613f5de3bd5be5356b5f5eba0ff2152e3c1359
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-04-11 15:49:42 +0530

    Create GFG SquareRootOfNumber.cpp

---
+++ GFG SquareRootOfNumber.cpp
@@ -0,0 +1,15 @@
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here   
+        if(x==0 || x==1)return x;
+        
+        long long result=1,i=1;
+        while(result<=x){
+            i++;
+            result=i*i;
+        }
+        return i-1;
+    }
+};
