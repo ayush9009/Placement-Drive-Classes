996c2d759a3eee2b8b06828963bb157d7128109d
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-08-19 22:29:44 +0530

    Update AmazonKClosestPointsToOrigin.cpp

--- AmazonKClosestPointsToOrigin.cpp
+++ AmazonKClosestPointsToOrigin.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
          vector<vector<int>>ans;
-         multimap<double,vector<int>>mp;
+         multimap<double,vector<int>>mp
 
         for(int i=0;i<points.size();i++){
             double res=pow(points[i][0],2)+pow(points[i][1],2);
